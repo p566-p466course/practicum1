9c88928c61738ee5d3e7e4684ec6d1bcd91eff25
Krisha Elle <krisharelle@gmail.com>
2025-04-12 02:24:53 -0400

    final push

--- scripts/push-to-ghcr.sh
+++ scripts/push-to-ghcr.sh
@@ -2,8 +2,11 @@
 set -e
 
 : "${VERSION:=v1.0.0}"
+
 echo "Logging into GitHub Container Registry..."
 echo "$ACCESS_TOKEN" | docker login ghcr.io -u krisha34 --password-stdin
 
-echo "Pushing image to ghcr.io/KRISHA/prime-service:${VERSION}..."
+echo "Pushing image to ghcr.io/krisha34/prime-service:${VERSION}..."
+docker tag prime-service:${VERSION} ghcr.io/krisha34/prime-service:${VERSION}
+
 docker push ghcr.io/krisha34/prime-service:${VERSION}
