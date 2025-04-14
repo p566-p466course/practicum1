fa4eb0501c6c962720a50d10568f9d758613da25
Krisha Elle <krisharelle@gmail.com>
2025-04-14 02:14:56 -0400

    Pushing the changed files

--- scripts/push-to-acr.sh
+++ scripts/push-to-acr.sh
@@ -10,7 +10,7 @@ echo "Logging into Azure Container Registry: $REGISTRY_URL"
 echo "$REGISTRY_PASSWORD" | docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" --password-stdin
 
 echo "Tagging image for ACR..."
-docker tag prime-service:latest $REGISTRY_URL/prime-service:$VERSION
+docker tag prime-service:latest $REGISTRY_URL/prime-service:latest
 
 echo "Pushing Docker image to ACR..."
 docker push $REGISTRY_URL/prime-service:$VERSION
